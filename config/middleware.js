98d1bdf926101cfbff0229df09a4972aace032d9
en3sis <en3sis@gmail.com>
2021-05-27 11:53:42 +0200

    Trying CORS

--- config/middleware.js
+++ config/middleware.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 
 const corsDomains = process.env.EOS_CORS_DOMAINS.split(', ')
+const heroku = process.env.HEROKU_INSTANCE
 
 module.exports = {
   settings: {
     cors: {
-      enabled: true,
-      origin: ['*']
+      origin: [`http:localhost, http:localhost:1337, http://localhost:${process.env.PORT || 1337}`, heroku,
+      ...corsDomains]
     }
   }
 };
