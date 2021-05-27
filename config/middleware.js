c00303af2938415cc70dd1d5e08f79e66bf3160f
en3sis <en3sis@gmail.com>
2021-05-27 10:35:46 +0200

    Test corst for localhost

--- config/middleware.js
+++ config/middleware.js
@@ -5,7 +5,8 @@ const corsDomains = process.env.EOS_CORS_DOMAINS.split(', ')
 module.exports = {
   settings: {
     cors: {
-      origin: [`http://localhost:${process.env.PORT || 3000}`, `localhost:${process.env.PORT || 3000}`,
+      enabled: true,
+      origin: [`http://localhost`,
       ...corsDomains]
     }
   }
