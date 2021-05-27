abdb0d193f8a97762b7d8e71966014bcff004c77
en3sis <en3sis@gmail.com>
2021-05-27 09:40:28 +0200

    Test all CORS

--- config/middleware.js
+++ config/middleware.js
@@ -5,8 +5,8 @@ const corsDomains = process.env.EOS_CORS_DOMAINS.split(', ')
 module.exports = {
   settings: {
     cors: {
-      origin: [`http://localhost:${process.env.PORT || 3000}`,
-      ...corsDomains]
-    }
+      enabled: true,
+      origin: ['*']
+    },
   }
 };
