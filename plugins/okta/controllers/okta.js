8339f0546c293fa58a2db6c99062e8a8757a4cdd
Hrishikesh Agarwal <hrishikeshagarwalv@gmail.com>
2022-07-20 03:23:58 +0530

    updated routes file
    Signed-off-by: Hrishikesh Agarwal <hrishikeshagarwalv@gmail.com>

--- plugins/okta/controllers/okta.js
+++ plugins/okta/controllers/okta.js
@@ -16,10 +16,9 @@ module.exports = {
 
   index: async (ctx) => {
     // Add your own logic here.
-
     // Send 200 `ok`
-    ctx.send({
-      message: 'ok'
-    });
+    return {
+      message: 'your message here'
+    }    
   }
 };
